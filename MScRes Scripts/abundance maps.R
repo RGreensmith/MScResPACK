60add56c8d612d4c2fdbacb5052476c98f40c0ef
RGreensmith <rose.greensmith@yahoo.co.uk>
2018-10-01 14:40:08 +0100

    cleaning script

--- MScRes Scripts/abundance maps.R
+++ MScRes Scripts/abundance maps.R
@@ -1,3 +1,29 @@
+
+##################################################################
+# Working directory #
+##################################################################
+
+rm(list=ls())
+# wd="C:/Users/Rose/Documents/Rose/"
+# wd="C:/Users/Tom/Documents/Rose_MScRes/Recent work backup/"
+# wd="C:/Users/Laptop User/Documents/"
+wd="M:/My Documents/Work/"
+setwd(wd)
+
+##################################################################
+# Data #
+###################################################################
+
+# formulas <- read.csv(paste(wd,"Copy of FormulasTest4 (2)",".csv",sep=""), stringsAsFactors=FALSE)
+# Ovexpl <- read.csv(paste(wd,"OVexpl_reduced_cols (2).csv",sep=""), stringsAsFactors=FALSE)
+# covFunTable <- read.csv(paste(wd,"covFunTable new cabund df (2).csv",sep=""), stringsAsFactors=FALSE)
+
+formulas = MScResPACK::formulas
+# Ovexpl = MScResPACK::Ovexpl
+# covFunTable = MScResPACK::covFunTable
+
+
+
 ##################################
 # Map of predicted #
 ##################################
@@ -19,8 +45,8 @@ for (m in c(2)) { # length(formulas$model_index)
   # Preparing abundance dataset #
   ############################################################
   
-  seasonsInd=seasonsIndFun(dataset)
-  dataset=prepDF(dataset)
+  # seasonsInd=seasonsIndFun(dataset)
+  # dataset=prepDF(dataset)
   
   #####################################################################
   # cap extreme values #
@@ -30,28 +56,37 @@ for (m in c(2)) { # length(formulas$model_index)
   
   sppColRef=formulas$dataset_name[m]
     
-  
+  ######################################################################
   # arguments #
+  ######################################################################
   
   fileNm = "BAT_raster"
   leglab = "Depth (m)"
   baseRefsDf = data.frame(fileNm,leglab)
-  
-  wdExtension = paste(Path,"Plots/",sep = "")
-  
-  mapName = paste(ModelRefNo," Map of abundance",sep = "")
-  
+
   legTOP = bquote("Abundance of " ~ .(formulas$Species[m])~ ~ abundance ~ (per ~ km^2))
+  mapsVis = "topWbaseCont"
+  basemapOutline = "Env_outline"
+  basemapDF = NULL
+  topmapDF = dataset[,sppColRef]
+  wdExtension = paste(Path,"Plots/",sep = "")
+  mapName = paste(ModelRefNo," Map of abundance",sep = "")
+  countOnly = TRUE
+  bubble = FALSE
   
-  topmapDF = 
-  
+  #######################################################################
   # create map #
+  #######################################################################
   
-  bathymetryR=mapFun(baseRefsDf = baseRefsDf, legTOP = legTOP,mapsVis = "both",
-                     basemapOutline = "Env_outline",
-                     basemapDF = NULL, topmapDF = predictedR, wdExtension = wdExtension,
-                     mapName = mapName,countOnly = FALSE,bubble = FALSE)
+  bathymetryR=mapFun(baseRefsDf, legTOP, mapsVis,basemapOutline,
+                     basemapDF, topmapDF, wdExtension,
+                     mapName,countOnly,bubble)
   
   rm(predictedR)
   
 }
+
+
+#######################################################
+# End #
+#######################################################
