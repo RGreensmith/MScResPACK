cd8872b9801e5cd4fa48f296430e0b24f4fa64f4
RGreensmith <rose.greensmith@yahoo.co.uk>
2018-10-02 18:10:09 +0100

    created wrapper for random walk metropolis sampling

---
+++ MScResPACK/R/metropRW.R
@@ -0,0 +1,61 @@
+#########################################################
+# Random walk metropolis sampling #
+#########################################################
+
+
+#' Random walk metropolis sampling wrapper function
+#'
+#' This function allows you to carry out Random Walk Metroplis Sampling on a model, and creates two plots.
+#' @param model model object of glmmTMB class
+#' 
+#' @param filePath file pathway, must end with "/" or beginning of the file name
+#' @keywords cats
+#' @export
+#' @examples
+#' filePath = paste(Path,"Plots/",ModelRefNo, sep = "")
+#' metropRW(model,filePath)
+#'
+
+metropRW = function(model,filePath) {
+  
+  if (is.null(model$sdr$par.random)) {
+    rawcoef = with(model$obj$env,last.par)
+  } else {
+    rawcoef <- with(model$obj$env,last.par[-random])
+  }
+  
+  
+  equalTest=all.equal(c(model$obj$fn(rawcoef)),
+                      -c(logLik(model)),
+                      tolerance=1e-7)
+  
+  summaryTable$metropEqual[st]=equalTest
+  
+  
+  fn <- function(x) -model$obj$fn(x)
+  V <- vcov(model,full=TRUE)
+  
+  s1 <- system.time(m1 <- try(MCMCmetrop1R(fn,rawcoef,V=V)))
+  
+  
+  colnames(m1) = dimnames(V)[[1]]
+  
+  png(filename=paste(filePath," metrop T and D %03d.png", sep = ""),width=1000,height=1000)
+  op=par(mfrow=c(4,2))
+  plot(m1,smooth = TRUE)
+  par(op)
+  dev.off()
+  
+
+  png(filename=paste(filePath," metrop aCorr %03d.png", sep = ""),width=1000,height=1000)
+  op=par(mfrow=c(4,2))
+  autocorr.plot(m1)
+  par(op)
+  dev.off()
+  
+  rm(m1)
+  
+}
+##############################################################
+# End #
+##############################################################
