c67b11f196d0614cc6a5821bcd9506ac3a7098af
RGreensmith <rose.greensmith@yahoo.co.uk>
2018-11-18 00:04:49 +0000

    adding option to alter range to be scaled to

--- MScResPACK/R/scaleFun.R
+++ MScResPACK/R/scaleFun.R
@@ -3,13 +3,17 @@
 #'
 #' This function allows you to scale a vector 0 to 1.
 #' @param x vector of values to scale
-#' @param a maximum value to be scaled to (i.e. 0 to a). Default value is a = 1.
+#' @param a minimum value to be scaled to. Default value is a = 0.
+#' @param b maximum value to be scaled to. Default value is a = 1.
 #' @keywords cats
 #' @export
 #' @examples
 #' scaleFun()
 #'
 
-scaleFun=function(x,a=1) {
-  (x-min(x))/(max(x)-min(x))*a
+scaleFun=function(x,a=0,b=1) {
+  
+  dif=b-a
+  (x-min(x))/(max(x)-min(x))*dif+a
+  
 }
